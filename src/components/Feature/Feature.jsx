66a562c6caf8078043b900d8ce0d703e44f1c6ec
ArifMiah07 <arifmiah.me322@gmail.com>
2024-04-04 09:33:36 +0600

    add images in feature section using json fake data

--- src/components/Feature/Feature.jsx
+++ src/components/Feature/Feature.jsx
@@ -1,16 +1,28 @@
-
+import PropTypes from 'prop-types'; // ES6
 
 const Feature = ({cat}) => {
     const {name, id, title, imgUrl} = cat;
     return (
-        <div className="max-w-sm rounded overflow-hidden shadow-lg">
-      <img className="w-full" src={imgUrl} alt={name} />
-      <div className="px-6 py-4">
-        <div className="font-bold text-xl mb-2">{name}</div>
-        <p className="text-gray-700 text-base">{title}</p>
-      </div>
-    </div>
+        <>
+            <div className=" flex flex-col justify-between max-w-sm rounded overflow-hidden shadow-lg">
+                <img className="w-full" src={imgUrl} alt={name} />
+                <div className="px-6 py-4">
+                    <div >
+                        <h1 className="font-bold text-xl mb-2">{name} <span><sup>{id}</sup></span></h1>
+                    </div>
+                    <p className="text-gray-700 text-base mb-3">{title}</p>
+                    <div>
+                        <button className="text-white font-semibold px-4 py-2 bg-pink-600 hover:bg-blue-600 rounded-xl">Buy Now</button>
+                    </div>
+                </div>
+            </div>
+        </>
     );
 };
 
+
+
+Feature.propTypes = {
+    cat: PropTypes.object
+}
 export default Feature;
--- src/components/Features/Features.jsx
+++ src/components/Features/Features.jsx
@@ -13,7 +13,7 @@ const Features = () => {
         .then(data => setCats(data));
     }, [])
     return (
-        <div>
+        <div className="w-fit mx-auto grid grid-cols-1 md:grid md:grid-cols-2 lg:grid lg:grid-cols-3 xl:grid xl:grid-cols-4 2xl:grid 2xl:grid-cols-5 gap-4 p-3">
             {
                 cats.map((cat, idx)=> <Feature key={idx} cat={cat}></Feature>)
             }
